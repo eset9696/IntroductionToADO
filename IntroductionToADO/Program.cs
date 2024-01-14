cf441ecdb4c36113663d7ca1a31d4815c4d1f75c
Mikhail Sherkunkov <sherkunkov@gmail.com>
2024-01-14 18:46:28 +0800

    1

--- IntroductionToADO/Program.cs
+++ IntroductionToADO/Program.cs
@@ -15,27 +15,14 @@ namespace IntroductionToADO
 			string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 			SqlConnection connection = new SqlConnection(connectionString);
 			connection.Open();
-			/*//string insertString = @"INSERT INTO Authors (first_name, last_name) VALUES('Stephen', 'Hawking')";
-			string insertString = @"INSERT INTO Books (author, title, price, pages) VALUES(4, 'The Theory of Everything', 160, 850)";
-			*/
-			/*SqlCommand insertCommand = new SqlCommand(insertString, connection);
-			insertCommand.ExecuteNonQuery(); // для insert, update, delete*//*
-			SqlCommand selectCommand = new SqlCommand(insertString, connection);
-			string selectString = @"SELECT * FROM Authors";
-			selectCommand.CommandText = selectString;
-			SqlDataReader rdr = selectCommand.ExecuteReader();
-			while (rdr.Read()) 
-			{
-				Console.WriteLine($"{rdr[0]} {rdr[1]} {rdr[2]}");
-			}*/
-			//selectAuthorsQuery(connection);
-			selectAuthorsAndBooksQuery(connection);
+			showMainMenu(connection);
 			connection.Close();
 		}
 
 		static void showMainMenu(SqlConnection connection)
 		{
-			Console.WriteLine("Выберите опцию: \n1 - Добавить нового автора\n2 - Добавить новую книгу\n3 - Добавить новую книгу и автора");
+			Console.WriteLine("Выберите опцию: \n1 - Добавить нового автора\n2 - Добавить новую книгу\n3 - Добавить новую книгу и автора\n4 - Показать всех авторов в базе" +
+				"\n5 - Показать все книги в базе\n6 - Удалить автора из базы\n7 - Удалить книгу из базы");
 			int option = Convert.ToInt32(Console.ReadLine());
 			chooseOptionOfMainMenu(connection, option);
 		}
@@ -59,6 +46,22 @@ namespace IntroductionToADO
 				string[] book = addBook();
 				addBookQuery(connection, book[0], book[1], book[3]);
 			}
+			else if (option == 4)
+			{
+				selectAuthorsQuery(connection);
+			}
+			else if (option == 5)
+			{
+				selectAuthorsAndBooksQuery(connection);
+			}
+			else if (option == 6)
+			{
+				deleteAuthorQuery(connection);
+			}
+			else if (option == 7)
+			{
+				selectAuthorsQuery(connection);
+			}
 		}
 
 		static string[] addAuthor()
@@ -79,7 +82,8 @@ namespace IntroductionToADO
 
 		static void addAuthorQuery(SqlConnection connection, string authorsFirstName, string authorsLastName) 
 		{
-			string newAuthor = $@"BEGIN INSERT INTO Authors (first_name, last_name) VALUES('{authorsFirstName}', '{authorsLastName}') WHERE NOT EXIST (SELECT * FROM Authors WHERE first_name = '{authorsFirstName}' AND last_name = '{authorsLastName}') END";
+			string newAuthor = $@"IF NOT EXISTS (SELECT * FROM Authors WHERE Authors.first_name = '{authorsFirstName}' AND Authors.last_name = '{authorsLastName}') BEGIN INSERT INTO Authors (first_name, last_name) VALUES('{authorsFirstName}', '{authorsLastName}') END";
+			
 			SqlCommand insertCommand = new SqlCommand(newAuthor, connection);
 			insertCommand.ExecuteNonQuery();
 		}
@@ -118,6 +122,14 @@ namespace IntroductionToADO
 				Console.WriteLine($"{rdr[0]} {rdr[1]}\t\t{rdr[2]}");
 			}
 			rdr.Close();
+		}
+
+		static void deleteAuthorQuery(SqlConnection connection)
+		{
+			Console.WriteLine("Введите id автора");
+			int id = Convert.ToInt32((Console.ReadLine()));	
+			string query = $@"DELETE FROM Authors WHERE Authors.id = {id}";
+			SqlCommand deleteCommand = new SqlCommand(query, connection);
 		}
 	}
 }
